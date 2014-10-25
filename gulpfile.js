39413baffd0f15ed2bb17c978edb08626d40d97e
Brent Jackson <jacksonblack@gmail.com>
2014-10-25 11:39:42 -0400

    Basic file structure

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+
+var gulp = require('gulp');
+var basswork = require('gulp-basswork');
+var mincss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+
+gulp.task('default', function() {
+  gulp.src('./src/base.css')
+    .pipe(basswork())
+    .pipe(rename('basscore.css'))
+    .pipe(gulp.dest('./css'))
+    .pipe(mincss())
+    .pipe(rename('basscore.min.css'))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', ['default'], function() {
+  gulp.watch(['./src/**/*'], ['default']);
+});
+
